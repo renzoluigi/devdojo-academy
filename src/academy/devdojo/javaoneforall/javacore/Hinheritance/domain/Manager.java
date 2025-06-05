3a6e910f77bea2560a213f0674d48e719f959214
Renzo Luigi <renzooluigi@hotmail.com>
2025-06-05 19:31:38 -0300

    class72

--- src/academy/devdojo/javaoneforall/javacore/Hinheritance/domain/Manager.java
+++ src/academy/devdojo/javaoneforall/javacore/Hinheritance/domain/Manager.java
@@ -1,11 +1,18 @@
 package academy.devdojo.javaoneforall.javacore.Hinheritance.domain;
 
-public class Manager extends Employee { // -> especialization
+public class Manager extends Employee { // -> specialization
     private String department;
 
     public Manager(String name, String socialSecurityNumber, Address address, double salary, String department) {
         super(name, socialSecurityNumber, address, salary);
         this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                "department='" + department + '\'' +
+                '}';
     }
 
     public String getDepartment() {
